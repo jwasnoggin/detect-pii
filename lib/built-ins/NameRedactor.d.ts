ebd2a7248d5a4f0e3dab05ea2ce3252a81713bbf
James Wassink <jwassink@noggin.com.au>
2024-01-22 10:34:24 +1100

    Remove lib, update package.json

--- lib/built-ins/NameRedactor.d.ts
+++
@@ -1,6 +0,0 @@
-import { ISyncRedactor } from '../types';
-export declare class NameRedactor implements ISyncRedactor {
-    private replaceWith;
-    constructor(replaceWith?: string);
-    redact(textToRedact: string): string;
-}
--- lib/composition.d.ts
+++
@@ -1,2 +0,0 @@
-import { AsyncCustomRedactorConfig, IAsyncRedactor, CompositeRedactorOptions, SyncCustomRedactorConfig, ISyncRedactor } from './types';
-export declare function composeChildRedactors<T extends AsyncCustomRedactorConfig>(opts?: CompositeRedactorOptions<T>): T extends SyncCustomRedactorConfig ? ISyncRedactor[] : (ISyncRedactor | IAsyncRedactor)[];
